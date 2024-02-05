18fa779fd7361dd309fe8dcb80c802f71af10470
ShishirDasNiloy <shishirdasniloy@gmail.com>
2024-02-06 00:45:56 +0600

    added: functionallity for small screen more info button

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// More info button functionality
+const elementToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
+
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+sidebarBtn.addEventListener("click", function () {
+  elementToggleFunc(sidebar);
+});
